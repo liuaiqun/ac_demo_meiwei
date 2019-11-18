26b0c741af9bb04b490fe6e6161f0c2d0e52d18e
antdesinger <kingbox@88v88v.com>
2019-11-18 20:33:08 +0800

    细化安装错误提示.添加城建税科目

--- __init__.py
+++ __init__.py
@@ -127,8 +127,8 @@ def add_accounts(env, rows):
         else:
             errorRows.append(row)
     if len(errorRows) > 0:
-        rl = [e[0] for e in errorRows]
-        raise UserError('以下科目出错,可能是因为没有上级科目,重复导入或已经存在\n'+'\n'.join(rl))
+        rl = ['_'.join(e) for e in errorRows]
+        raise UserError('以下科目出错,不能完成任务,可能是因为没有上级科目,重复导入或已经存在\n'+'\n'.join(rl))
         # 添加明细科目和核算项目-结束
 
         # 添加期初记录
